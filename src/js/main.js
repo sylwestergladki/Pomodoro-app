5ffceed95cca39ad39f1cb7335fa5d0488285241
sylwestergladki <sylwester.gladki@gmail.com>
2019-10-21 11:14:55 +0200

    feat: JS TEST

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,31 @@ if ('serviceWorker' in navigator) {
 }
 
 // place your code below
+let countdown;
 
+const timerDisplay = document.querySelector('.clock');
 
+function timer(seconds){
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
 
+  coutdown = setInterval(() =>{
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if(secondsLeft <= 0){
+      clearInterval(countdown);
+      return;
+    }
+
+    displayTimeLeft(secondsLeft);
+  }, 1000)
+}
+
+function displayTimeLeft(seconds){
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
