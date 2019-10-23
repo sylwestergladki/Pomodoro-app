5c3fb4e9bed8cdb4c0e0755fa130bcf8402f64b0
sylwestergladki <sylwester.gladki@gmail.com>
2019-10-23 08:58:22 +0200

    feat: JS update

--- src/js/main.js
+++ src/js/main.js
@@ -15,31 +15,38 @@ if ('serviceWorker' in navigator) {
 }
 
 // place your code below
-let countdown;
 
+let countdown;
+const start = document.querySelector('#startTimer');
 const timerDisplay = document.querySelector('.clock');
 
-function timer(seconds){
+function timer(seconds) {
+ 
+  // clear any existing timers
+  clearInterval(countdown);
+  seconds = 60;
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
 
-  coutdown = setInterval(() =>{
+  countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-
-    if(secondsLeft <= 0){
+    // check if we should stop it!
+    if(secondsLeft < 0) {
       clearInterval(countdown);
       return;
     }
-
+    // display it
     displayTimeLeft(secondsLeft);
-  }, 1000)
+  }, 1000);
 }
 
-function displayTimeLeft(seconds){
+function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
   document.title = display;
-  timerDisplay.textContent = display;
+  timerDisplay.innerHTML = display;
 }
+
+start.addEventListener('click', timer);
